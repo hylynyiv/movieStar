db6f943785db769a06483550fe42c18893d0a4b5
hylynyiv <info@hylynyiv.com>
2023-09-20 18:45:23 +0200

    Adding Home and Search

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Movies List</h1>
+    <ul>
+      <li v-for="movie in movies" :key="movie.id">
+        {{ movie.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      movies: JSON.parse(localStorage.getItem('movies') || '[]')
+    };
+  }
+};
+</script>
---
+++ src/components/SearchMovie.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <h2>Search Movies by Title</h2>
+        <input type="text" v-model="keyword" placeholder="Enter keyword">
+        <br>
+        <input type="text" v-model="API_Key" placeholder="Enter your OMDB API key">
+        <br>
+        <button @click="searchMovies">Search</button>
+
+        <ul v-if="movies && movies.length">
+            <li v-for="movie in movies" :key="movie.imdbID">
+               <h3> {{ movie.Title }} ({{ movie.Year }}) </h3>
+               <img :src="movie.Poster" alt="Movie Poster" v-if="movie.Poster !== 'N/A'"/>
+            <button @click="addToWatchList">Add to my watch list</button>
+            </li>
+        </ul>
+        <p v-if="error"> {{ error }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                keyword: "",
+                API_Key: "",
+                movies: [],
+                error: ""
+
+            }
+        },
+        methods: {
+            async searchMovies(){
+                try{
+                    const response = await fetch (
+                        `https://www.omdbapi.com/?s=${this.keyword}&type=movie&apikey=${this.API_Key}`
+                    );
+
+                    const data = await response.json();
+
+                    if(data.Response === "True"){
+                        this.movies = data.Search;
+                        this.error = "";
+                    } else {
+                        this.movies = [];
+                        this.error = data.Error;
+                    }
+                }catch (error){
+                this.error = "Failed to fetch movies.";
+                }
+            }
+
+        }
+
+    }
+</script>
+
+<style>
+</style>
